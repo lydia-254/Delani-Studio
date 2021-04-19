e70ee4856112d270496c7dec11dd41464aed298d
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-04-19 15:42:40 +0300

    Add changes on html

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+      $("#design-words").toggle();
+      $("#design-image").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#development").click(function(){
+      $("#development-words").toggle();
+      $("#development-image").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#product-management").click(function(){
+      $("#product-management-words").toggle();
+      $("#product-management-image").toggle();
+    });
+  });
